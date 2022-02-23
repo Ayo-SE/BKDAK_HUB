1dd01bf8927ea5a53043caaf9252529879d606fd
Kwabena Darkwa <darkwak@miamioh.edu>
2022-02-23 12:37:46 -0500

    Update App.js

--- group28_project/bkdak-hub/src/App.js
+++ group28_project/bkdak-hub/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edgkghjkghjsit <code>src/App.js</code> and save to reload.
+          blahhh <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
