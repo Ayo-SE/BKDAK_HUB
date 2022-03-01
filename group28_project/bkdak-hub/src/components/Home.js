6f57c285da785079b8a9ff844b406b68ca0ec04e
ka8er <swearikj@miamioh.edu>
2022-02-28 21:39:08 -0500

    Update Home.js

--- group28_project/bkdak-hub/src/components/Home.js
+++ group28_project/bkdak-hub/src/components/Home.js
@@ -7,28 +7,35 @@ export default class Home extends Component {
       <div className='p-5'>
         <div className='top-pads'>
         <div className='d-flex justify-content-around'>
-          <div className='card' style={{width: "18rem"}}>
+          <div className='card' style={{width: "19rem"}}>
             <div className='card-body'>
               <h5 className="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
             </div>
           </div>
 
-          <div className='card' style={{width: "18rem"}}>
+          <div className='card' style={{width: "19rem"}}>
             <div className='card-body'>
               <h5 className="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. </p>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. bleh</p>
             </div>
           </div>
 
-          <div className='card' style={{width: "18rem"}}>
+          <div className='card' style={{width: "19rem"}}>
+            <div className='card-body'>
+              <h5 className="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. bleh</p>
+            </div>
+          </div>
+
+          <div className='card' style={{width: "19rem"}}>
             <div className='card-body'>
               <h5 className="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
 
-          <div className='card' style={{width: "18rem"}}>
+          <div className='card' style={{width: "19rem"}}>
             <div className='card-body'>
               <h5 className="card-title">Card title</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
