a7380e7ad38e86c4945ab12efc6458782311a9c7
ka8er <swearikj@miamioh.edu>
2022-03-03 15:01:45 -0500

    Update Home.js

--- group28_project/bkdak-hub/src/components/Home.js
+++ group28_project/bkdak-hub/src/components/Home.js
@@ -34,21 +34,7 @@ export default class Home extends Component {
             </div>
           </div>
 
-          <div className='card' style={{width: "19rem"}}>
-            <div className='card-body'>
-              <h5 className="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-
-          <div className='card' style={{width: "19rem"}}>
-            <div className='card-body'>
-              <h5 className="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
-
-          </div>
+           </div>
         </div>
       </div>
     )
