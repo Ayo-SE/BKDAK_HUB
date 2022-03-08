d91ed90da0ef5a960b130079349d49cf2ef8e19b
Kwabena Darkwa <darkwak@miamioh.edu>
2022-03-08 15:50:30 -0500

    Help page updates

--- group28_project/bkdak-hub/src/components/Help/Help.js
+++ group28_project/bkdak-hub/src/components/Help/Help.js
@@ -6,8 +6,36 @@ export default class Help extends Component {
     return (
 
 
+        <div class ="general">
+            <div class="menu"> 
+                <a href="#"> BKDAK Inc. </a>
+                <a href="#"> Contact </a>
+                <a href="#"> Support </a>
+            </div> 
+
+            <div class="body"> 
+                <div>
+                    <h2 id= "BKDAK Inc"> BKDAK Inc.</h2>
+                    <p> Who are we? We are a small company founded in 2016. We strive in creating working software in all </p>
+                </div>
+
+                <div>
+                    <h2 id="Contact"> Contact </h2>
+                </div>
+
+                <div>
+                    <h2 id="Support"> Support </h2>
+                </div>
 
 
+                <h1> Slidebar Using HTML and CSS </h1>
+                <p> Who are we? We are a small company founded in 2016. We strive in creating working software in all </p> 
+
+            </div>
+
+        </div>
+
+        /*
         <div> 
 
            
@@ -36,7 +64,7 @@ export default class Help extends Component {
         </div>
 
         
-      
+      */
       
 
 
