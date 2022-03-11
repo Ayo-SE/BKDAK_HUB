700356b9e846edd66fa668b14fba2f47b025743a
Kwabena Darkwa <darkwak@miamioh.edu>
2022-03-11 03:46:51 -0500

    Updates to help

--- group28_project/bkdak-hub/src/components/Help/Help.js
+++ group28_project/bkdak-hub/src/components/Help/Help.js
@@ -8,28 +8,91 @@ export default class Help extends Component {
 
         <div class ="general">
             <div class="menu"> 
-                <a href="#"> BKDAK Inc. </a>
-                <a href="#"> Contact </a>
-                <a href="#"> Support </a>
+                <a href="#BKDAK Inc"> BKDAK Inc. </a>
+                <a href="#mission"> Mission  </a>
+                <a href="#Contact"> Contact </a>
+                <a href="#Support"> Support </a>
             </div> 
 
             <div class="body"> 
                 <div>
                     <h2 id= "BKDAK Inc"> BKDAK Inc.</h2>
-                    <p> Who are we? We are a small company founded in 2016. We strive in creating working software in all </p>
+                    <hr></hr>
+
+                    <p> Who are we? We are a small company founded in 2017 in Chicago Illinois. We strive in creating efficient 
+                    software that meets our all of our customers needs and wants. Our mission, Spirit, values and behavior 
+                    are what separates us from other companies. We are committed to building and cultivating postive, long
+                    term relationships with all our clients.  </p>
+                </div>
+
+
+                <div> 
+                    <h2 id="mission"> Mission Statement </h2>
+                    <hr></hr>
+
+                    <p>  Our mission is simple: make technology an asset for your business not a problem. 
+                        Too many times we have seen other IT consulting firms recommend solutions that are not appropriate for 
+                        the specific client’s business or will not take a business’ operations into their planning as they routinely 
+                        bring down an entire business while they perform routine maintenance on a server. That is unacceptable. 
+                        We won’t bring you down in the middle of the day because it is “a better time for us”. We work around your business. 
+                        We strive to make technology integrate seamlessly with your business so your business can grow. As your technology partner, 
+                        when your business grows ours will grow with you, therefore, we will work hand in hand with you to support your growth.
+                    </p>
+
+
                 </div>
 
                 <div>
                     <h2 id="Contact"> Contact </h2>
+                    <hr></hr>
+                    
+                    <p> Project Manager: Brandon Truong <br></br> 
+                        Email: truongbm@miamioh.edu 
+                        <br></br>
+                        <br></br>
+
+                        Technical Manager: Dillon Khanna <br></br>
+                        Email: khannadf@miamioh.edu 
+                        <br></br>
+                        <br></br>
+                       
+                        <div> 
+                        Software Developer: Ayo Obisesan <br></br>
+                        Email: obisesa2@miamioh.edu 
+                        <br></br>
+                        <br></br>
+                        
+                        Software Developer: Kaitlyn Swearingen <br></br>
+                        Email: swearikj@miamioh.edu 
+                        <br></br>
+                        <br></br>
+                        
+                        Software Developer: Kwabena Darkwa <br></br>
+                        Email: Darkwak@miamioh.edu 
+                        
+                        </div>
+                        
+                    </p>
                 </div>
 
                 <div>
                     <h2 id="Support"> Support </h2>
+                    <hr></hr>
+
+                    <p> For assistance with any products or software, please feel free to send us an email provided 
+                        below or contact our hotline.
+                        <br></br>
+                        <br></br> 
+                        1-800-BKDAKHELP 
+                        <br></br>
+                        <br></br>
+                        BKDAK@gmail.com 
+                        </p> 
+
+                        
+
                 </div>
 
-
-                <h1> Slidebar Using HTML and CSS </h1>
-                <p> Who are we? We are a small company founded in 2016. We strive in creating working software in all </p> 
 
             </div>
 
