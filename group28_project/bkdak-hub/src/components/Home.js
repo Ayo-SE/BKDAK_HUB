e18d615fa4aa6f75af214391fda36b7427fb5f85
Ayo Obisesan <77123546+dark1kn1ght@users.noreply.github.com>
2022-03-30 12:18:18 -0400

    images added to page

--- group28_project/bkdak-hub/src/components/Home.js
+++ group28_project/bkdak-hub/src/components/Home.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import './Home.css';
 import snap from './images/snapchat.png';
+import insta from './images/Instagram_icon.png';
+import lyft from './images/lyft.png';
+
 import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
@@ -88,17 +91,27 @@ export default class Home extends Component {
               </div>
 
               <div className='card' style={{width: "16rem"}}>
-                <div className='card-body'>
-                  <h5 className="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. bleh</p>
-                </div>
+                <img src={insta} class="card-img-top" alt="instagram-image"></img>
+                  <div className='card-body'>
+                    <h5 className="card-title">Instagram</h5>
+                    <p class="card-text">Instagram is a fast and fun way to share the moment with your friends and family.</p>
+                    <div className='d-flex justify-content-evenly'>
+                      <a href="#" class="btn btn-primary">Share</a>
+                      <a href="#" class="btn btn-primary">Review</a>
+                    </div>
+                  </div>
               </div>
 
               <div className='card' style={{width: "16rem"}}>
-                <div className='card-body'>
-                  <h5 className="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. bleh</p>
-                </div>
+              <img src={lyft} class="card-img-top" alt="lyft-image"></img>
+                  <div className='card-body'>
+                    <h5 className="card-title">Lyft</h5>
+                    <p class="card-text">Lyft is... </p>
+                    <div className='d-flex justify-content-evenly'>
+                      <a href="#" class="btn btn-primary">Share</a>
+                      <a href="#" class="btn btn-primary">Review</a>
+                    </div>
+                  </div>
               </div>
 
               <div className='card' style={{width: "16rem"}}>
